4f33fdb53d1332909631cba963823a06c6b85aa7
THERISINGSUN <36433536+surajpanker@users.noreply.github.com>
2020-05-12 08:28:32 +0530

    Update and rename Easy/README.md to sameTree.java

--- sameTree.java
+++ sameTree.java
@@ -1,3 +1,4 @@
+/*
 Given two binary trees, write a function to check if they are the same or not.
 
 Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
@@ -29,7 +30,10 @@ Input:     1         1
         [1,2,1],   [1,1,2]
 
 Output: false
-class Solution {
+
+*/
+class Solution 
+{
     public boolean isSameTree(TreeNode p, TreeNode q) {
     if(p==null && q==null){
         return true;
